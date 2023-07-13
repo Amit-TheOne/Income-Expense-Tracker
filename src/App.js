e0b8ab51bf633267f7e11c50536a5c171e116f42
Amit-TheOne <amit.dew009@gmail.com>
2023-07-13 16:27:48 +0530

    Feat: Voice input using Speechly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel, IntroPopup, TranscriptDrawer } from '@speechly/react-ui'
 
 import Main from './components/Main/Main'
 import Details from './components/Details/Details'
@@ -28,6 +29,12 @@ const App = () => {
           <Details title="Expense" />
         </Grid>
       </Grid>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+        {/* <ErrorPanel /> */}
+        {/* <TranscriptDrawer />
+        <IntroPopup /> */}
+      </PushToTalkButtonContainer>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { Provider } from "./context/context";
+import { SpeechProvider } from "@speechly/react-client";
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
+    <SpeechProvider appId="4e80870f-a77b-44d8-a7ce-150fb7f164bf" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
     document.getElementById("root")
 );
