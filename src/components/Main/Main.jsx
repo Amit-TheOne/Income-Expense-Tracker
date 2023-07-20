cc90d0d679e9e5c12b86cdd7ec21f7bb582c94a7
Amit-TheOne <amit.dew009@gmail.com>
2023-07-20 16:02:02 +0530

    Added: Local Storage

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
-  Card,
-  CardHeader,
-  CardContent,
-  Typography,
-  Grid,
-  Divider,
+    Card,
+    CardHeader,
+    CardContent,
+    Typography,
+    Grid,
+    Divider,
 } from "@material-ui/core";
+import { ExpenseTrackerContext } from "../../context/context";
 
 import useStyles from "./styles";
 import Form from "./Form/Form";
 import List from "./List/List";
 
 const Main = () => {
-  const classes = useStyles();
+    const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
-      <CardContent>
-        <Typography align="center" variant="h5">
-          Total Balance $100
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          style={{ lineheight: "1.5rem", marginTop: "20px" }}
-        >
-          {/* InfoCard */}
-          Try saying: Add income for $100 in Category Salary for Monday...
-        </Typography>
-        <Divider />
-        <Form />
-      </CardContent>
-      <CardContent className={classes.cardContent}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <List />
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                title="Expense Tracker"
+                subheader="Powered by Speechly"
+            />
+            <CardContent>
+                <Typography align="center" variant="h5">
+                    Total Balance ${balance}
+                </Typography>
+                <Typography
+                    variant="subtitle1"
+                    style={{ lineheight: "1.5rem", marginTop: "20px" }}
+                >
+                    {/* InfoCard */}
+                    Try saying: Add income for $100 in Category Salary for
+                    Monday...
+                </Typography>
+                <Divider />
+                <Form />
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <List />
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default Main;
--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { createContext, useReducer } from "react";
 
 import contextReducer from "./contextReducer";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem("transactions")) || [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -18,11 +18,14 @@ export const Provider = ({ children }) => {
         dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
     }
 
+    const balance = transactions.reduce((acc, currVal) => currVal.type === "Expense" ? acc - currVal.amount : acc + currVal.amount, 0);
+
     return (
         <ExpenseTrackerContext.Provider value={{
             addTransaction,
             deleteTransaction,
-            transactions
+            transactions,
+            balance
         }} >
             {children}
         </ExpenseTrackerContext.Provider >
